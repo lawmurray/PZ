19656861b60990fdee99c2b36dc3bf259f3e4783
Lawrence Murray <lawrence.murray@csiro.au>
2011-08-01 09:54:28 +0000

    Updated color bars and axis labels in plot_acceptance()

--- octave/plot_acceptance.m
+++ octave/plot_acceptance.m
@@ -12,17 +12,21 @@
 function plot_acceptance ()
     % load models from prepare_acceptance()
     load model_acceptance.mat
-    models{11} = models{3};
-    models{12} = models{6};
+    amodels = models;
+    load model_posterior.mat
+    pmodels = models;
+    
+    % use grey
+    colormap(flipud(gray));
     
     % common axes
     ax1 = [];
     cax1 = [];
     
     % first drawing
-    for i = 1:length(models)
+    for i = 1:length(amodels)
         subplot(2,6,i);
-        contour_model(models{i});
+        contour_model(amodels{i});
         ax1 = [ ax1; axis() ];
         cax1 = [ cax1; caxis() ];
     end
@@ -30,17 +34,25 @@ function plot_acceptance ()
     % second drawing, tidy
     ax2 = [ min(ax1(:,1)) max(ax1(:,2)) min(ax1(:,3)) max(ax1(:,4)) ];
     cax2 = [ max(min(cax1(:,1)),0) min(max(cax1(:,2)),1) ];
-    lvl2 = linspace(0, 1, 11);
-    for i = 1:length(models)
+    lvl2 = linspace(0, 1, 16);
+    for i = 1:length(amodels)
         subplot(2,6,i);
-        contour_model(models{i}, [], [0.28356237896608, 0.0969718952604018], ax2, lvl2);
+        contour_model(amodels{i}, [], [0.2836, 0.0970], ax2, lvl2);
+        hold on;
+        contour_model(models{mod(i - 1, length(pmodels)) + 1});
         %surf_model(models{i}, ax2);
         plot_defaults;
         axis(ax2);
         %axis([ ax2 cax2 ]);
-        caxis(cax2);
-        xlabel('{\mu}');
-        ylabel('{\sigma}');
-        colorbar;
+        caxis([0 1]);
+        if i == 1 || i == 7
+            ylabel('{\sigma}');
+        end
+        if i > 6
+            xlabel('{\mu}');
+        end
+        if i == 6 || i == 12
+            colorbar('east');
+        end
     end
 end
