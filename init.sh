d9526fad6bc67a714ae5d8daf38beae59fe37a92
Lawrence Murray <lawrence.murray@csiro.au>
2013-04-22 14:00:08 +0000

    Updated for recent LibBi changes

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-bi sample --target joint --model-file PZ.bi --end-time 100 --noutputs 100 --output-file data/obs.nc
+# simulate a dense data set
+bi sample @config.conf --target joint --model-file PZ.bi --end-time 100 --noutputs 100 --output-file results/joint.nc --seed -1
+
+# extract a sparse data set using OctBi
+rm -f data/obs.nc
+octave -q --eval "bi_sparsify_var('results/joint.nc', 'data/obs.nc', 'P_obs')"
